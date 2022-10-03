eaa3dda33d852b4d13c8225ffdf29417fa8729ba
Vinay Hegde <76780478+vinayhegde105@users.noreply.github.com>
2022-10-03 13:15:44 +0530

    Update apache2.sh

--- apache2.sh
+++ apache2.sh
@@ -5,6 +5,6 @@ sudo apt install apache2 -y
 sudo ufw app list
 sudo ufw allow 'Apache'
 sudo systemctl status apache2
-sudo chown ${USER}:${USER} /var/www/html
+sudo chown azureuser:azureuser /var/www/html
 # cd /var/www/html
 # rm index.html
