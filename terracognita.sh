b60264868a046ca20e07defaf86713b0d987b2ff
Vinay Hegde <76780478+vinayhegde105@users.noreply.github.com>
2022-10-25 17:58:55 +0530

    Update terracognita.sh

--- terracognita.sh
+++ terracognita.sh
@@ -2,6 +2,7 @@
 
 # url: https://github.com/cycloidio/terracognita.git
 #Sample Command: terracognita aws --hcl ec2 --tfstate terraform.tfstate --aws-default-region us-east-1 -i aws_instance
+#Sample Command: terracognita google --project cycloid-sandbox --region us-central1 --credentials /home/mathieu/cycloid/google/cycloid-iam-9789b361a19b.json --tfstate resources.tfstate --hcl resources.tf -i google_compute_instance -i google_compute_network
 
 
 #Command to install terracognita
